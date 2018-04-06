576cb2d50a9857d1bd57d59acf1df82a217c0c19
bilginm <fatihbilgin@gmail.com>
2018-04-06 16:15:26 +0300

    removed ws added new methods to API

---
+++ bitexen_client_settings.py
@@ -0,0 +1,8 @@
+key = 'T-SxdFyOd7mzpzsutUvP9A'
+secret = '2Ow_G6BbOfzB12i7rMW6gg'
+api_uri = 'http://dev.bitexen.com'
+pass_phrase = '1453'
+username = 'fatih.bilgin@bitexen.com'
+
+LOG_LEVEL = 10
+wsUri = 'http://dev.bitexen.com'
