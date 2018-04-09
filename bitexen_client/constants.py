aa48ce9935c340953ec4227a41f4c4d400246c61
bilginm <fatihbilgin@gmail.com>
2018-04-09 12:48:30 +0300

    added retry for error code 429 added constants

---
+++ bitexen_client/constants.py
@@ -0,0 +1,18 @@
+class BuySellType():
+    BUY = "B"
+    SELL = "S"
+
+class OrderStatus():
+    NEW = "N"
+    PARTIALFILLED = "P"
+    FILLED = "F"
+    CANCELED = "C"
+    REJECTED = "R"
+
+class OrderType():
+    LIMIT_ORDER = "limit"
+    STOP_ORDER = "stop"
+    MARKET_ORDER = "market"
+    CANCEL_ORDER = "cancel"
+    CANCEL_ALL_ORDER = "cancel_all" 
+
